c3c46ff91f064febfc671067e7f74c5a71ba3649
Bader <baderalsabbagh@outlook.com>
2023-04-02 17:02:03 +0300

    Added Tab Bar and Item Storyboard

---
+++ SomaApp/BiddingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BiddingViewController.swift
+//  SomaApp
+//
+//  Created by iOSdev on 29/03/2023.
+//
+
+import UIKit
+
+class BiddingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
