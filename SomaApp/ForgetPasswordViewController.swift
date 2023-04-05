701a0a6d65e54a87e05a97a5f694cc6741cf0028
isa <3isaburshaid@gmail.com>
2023-04-05 15:46:00 +0300

    added forget password feature // fully functional sends email to you

--- SomaApp/ForgetPasswordViewController.swift
+++ SomaApp/ForgetPasswordViewController.swift
@@ -1,29 +1,70 @@
-//
-//  ForgetPasswordViewController.swift
-//  SomaApp
-//
-//  Created by iOSdev on 30/03/2023.
-//
-
+ 
 import UIKit
+import Firebase
 
-class ForgetPasswordViewController: UIViewController {
+class ForgotPasswordViewController: UIViewController {
+    
+    
 
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        sendButton.layer.cornerRadius = 20
+        
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var email: UITextField!
+    
+   
+       
+    @IBAction func resetButton(_ sender: UIButton) {
+        
+        guard let emaill = email.text, !emaill.isEmpty  else {
+                    print("Missing field data")
+        
+                    let alert = UIAlertController(title: "Missing field data", message: "Please fill in the required fields", preferredStyle: .alert)
+        
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+                    self.present(alert, animated: true)
+        
+                    return
+                }
+        
+                Auth.auth().sendPasswordReset(withEmail: email.text!) {(error) in
+        
+                    if error == nil {
+        
+                        print("Check your Email")
+                        let alert = UIAlertController(title: "Check your Email", message: "Email has been sent to you!", preferredStyle: .alert)
+        
+                        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+                        self.present(alert, animated: true)
+        
+        
+                        return
+        
+                    } else {
+                        print("Invalid credentials!")
+                        let alert = UIAlertController(title: "Invalid credentials!", message: "Invalid credentials, please try again", preferredStyle: .alert)
+        
+                        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+                        self.present(alert, animated: true)
+                        return
+                    }
+                }
+        
+                email.text?.removeAll()
+            }
+        }
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
