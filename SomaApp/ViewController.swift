8273cd7ef02ec9a82f7cfce3ff2773558b817276
Raneem <raneemalawadhi@gmail.com>
2023-04-11 16:41:10 +0300

    added profile, edit profile, countries page, linked the pages to the settings

--- SomaApp/ViewController.swift
+++ SomaApp/ViewController.swift
@@ -9,13 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var tableView: UITableView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
 }
-
