ce4108764cdad02a1e12961d7e734d89cd008068
BaderAlsabbagh <108470273+BaderAlsabbagh@users.noreply.github.com>
2023-04-20 01:30:24 +0300

    Added Bid Submission and Time Left Functionality

--- SomaApp/StartBidTableViewController.swift
+++ SomaApp/StartBidTableViewController.swift
@@ -11,24 +11,157 @@ class StartBidTableViewController: UITableViewController {
     
     @IBOutlet weak var productImageView: UIImageView!
     
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var currentBidLabel: UILabel!
+    @IBOutlet weak var timeLeftLabel: UILabel!
+    
+    let calendar = Calendar.current
+    let dateFormatter = DateFormatter()
+    var endDate: Date?
+    var countdownTimer: Timer?
+    
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        currentBidLabel.text = UpdateBid.updatedBid.bid
+    //    }
+    //    override func viewDidDisappear(_ animated: Bool) {
+    //        currentBidLabel.text = UpdateBid.updatedBid.bid
+    //    }
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        
         let imageUuid = "tissotWatch.png"
-                ImportImage.shared.downloadImage(imageUuid: imageUuid) { [weak self] (image, error) in
-                    if let error = error {
-                        print("Error downloading image: \(error.localizedDescription)")
-                    } else if let image = image {
-                        self?.productImageView.image = image
-                    }
-                }
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func unwindToBidView(_ unwindSegue: UIStoryboardSegue) {
-            let sourceViewController = unwindSegue.source
-            // Use data from the view controller which initiated the unwind segue
+        ImportImage.shared.downloadImage(imageUuid: imageUuid) { [weak self] (image, error) in
+            if let error = error {
+                print("Error downloading image: \(error.localizedDescription)")
+            } else if let image = image {
+                self?.productImageView.image = image
+            }
         }
+        Database.Products["-NSs7VtqFlA-Ef7pKJ7u/productName"].getData { error, snapshotProductName in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return;
+            }
+            if let snapshotProductName = snapshotProductName?.value as? String {
+                
+                self.productNameLabel.text = snapshotProductName
+            }
+        }
+        Database.Products["-NSs7VtqFlA-Ef7pKJ7u/description"].getData { error, snapshotDescription in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return;
+            }
+            if let snapshotDescription = snapshotDescription?.value as? String {
+                
+                self.productDescriptionLabel.text = snapshotDescription
+            }
+        }
+        Database.Products["-NSs7VtqFlA-Ef7pKJ7u/currentBid"].getData { error, snapshotCurrentBid in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return;
+            }
+            if let snapshotCurrentBid = snapshotCurrentBid?.value as? String {
+                
+                self.currentBidLabel.text = snapshotCurrentBid + " BHD"
+            }
+        }
+        
+        // Bid updater
+        let productRef = Database.Products.products.child("-NSs7VtqFlA-Ef7pKJ7u")
+        
+        // Observe changes in the product node
+        productRef.observe(.value) { (snapshot) in
+            
+            // Get the product data from the snapshot
+            let productData = snapshot.value as? [String: Any] ?? [:]
+            
+            
+            // Get the current bid and update the label
+            let currentBid = productData["currentBid"] as? String ?? "N/A"
+            self.currentBidLabel.text = "\(currentBid) BHD"
+        }
+        
+        // Countdown
+        let calendar = Calendar.current
+        // Set the end date based on the selected duration
+        endDate = Date().addingTimeInterval(getTimeInterval(forDuration: .threeDays))
+        
+        // Set up date formatter
+        dateFormatter.dateFormat = "HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        
+        // Start the countdown timer
+        startCountdownTimer()
+    }
+    
+    // Start the countdown timer
+    func startCountdownTimer() {
+        countdownTimer?.invalidate()
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimeLeft), userInfo: nil, repeats: true)
+    }
+    
+    // Calculate and update the time left label
+    @objc func updateTimeLeft() {
+        guard let endDate = endDate else {
+            return
+        }
+        let currentDate = Date()
+        let components = calendar.dateComponents([.hour, .minute, .second], from: currentDate, to: endDate)
+        if let hours = components.hour, let minutes = components.minute, let seconds = components.second {
+            timeLeftLabel.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        }
+        if currentDate >= endDate {
+            countdownTimer?.invalidate()
+            timeLeftLabel.text = "00:00:00"
+        }
+    }
+    
+    // Get the time interval for the selected duration
+    func getTimeInterval(forDuration duration: Duration) -> TimeInterval {
+        switch duration {
+        case .oneHour:
+            return 3600
+        case .threeHours:
+            return 10800
+        case .sixHours:
+            return 21600
+        case .twelveHours:
+            return 43200
+        case .oneDay:
+            return 86400
+        case .threeDays:
+            return 259200
+        case .sevenDays:
+            return 604800
+        }
+    }
+    
+    // Enum for duration options
+    enum Duration {
+        case oneHour
+        case threeHours
+        case sixHours
+        case twelveHours
+        case oneDay
+        case threeDays
+        case sevenDays
+    }
+    
+    
+    
+    @IBAction func unwindToBidView(_ unwindSegue: UIStoryboardSegue) {
+        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
 }
+   
+
+
+
 //
 //    @IBAction func submitBidButton(_ sender: UIButton) {
 //
