c3cbefbb7f2a5075fd4c548151f781c8ab5ca225
GhadeerShehab <ghadeershehabb@gmail.com>
2023-04-20 16:23:31 +0300

    so many things

---
+++ SomaApp/CategoriesItemsCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CategoriesItemsCollectionViewCell.swift
+//  SomaApp
+//
+//  Created by iOSdev on 20/04/2023.
+//
+
+import UIKit
+
+class CategoriesItemsCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var categoryItemImage: UIImageView!
+    
+    @IBOutlet var categoryItemTitle: UILabel!
+    
+    @IBOutlet var categoryItemTimeLeft: UILabel!
+    
+    @IBOutlet var categoryItemDescription: UILabel!
+    
+    
+}
