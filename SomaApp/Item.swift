94dd6abf8bf09b700cdeaaf3b8620be2ff66b7d9
GhadeerShehab <ghadeershehabb@gmail.com>
2023-04-20 16:32:44 +0300

    item thing

---
+++ SomaApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SomaApp
+//
+//  Created by iOSdev on 20/04/2023.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var image : UIImage
+    var itemName : String
+    var timeLeft: String
+    var description: String
+    
+    
+}
