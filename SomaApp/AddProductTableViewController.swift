dbac45ccfae4aa88422432e9d4f63e46ffb54dc1
BaderAlsabbagh <108470273+BaderAlsabbagh@users.noreply.github.com>
2023-04-22 07:05:23 +0300

    Saved imageUuid in the products Data in Firebase

--- SomaApp/AddProductTableViewController.swift
+++ SomaApp/AddProductTableViewController.swift
@@ -6,17 +6,18 @@
 //
 import UIKit
 import Firebase
+import FirebaseStorage
 
 class AddProductTableViewController: UITableViewController, UIImagePickerControllerDelegate, UIPickerViewDelegate, UINavigationControllerDelegate, UIPickerViewDataSource {
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
- 
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
     // Product Listing Outlets (TEXT FIELDS)
-  
+    
     @IBOutlet weak var productNameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     @IBOutlet weak var startingPriceTextField: UITextField!
@@ -34,16 +35,20 @@ class AddProductTableViewController: UITableViewController, UIImagePickerControl
     @IBOutlet weak var checkMark: UIButton!
     @IBOutlet weak var submitItem: UIButton!
     
+    var imageView: UIImageView?
     // Array of image views to display in the scroll view
     var imageViews = [UIImageView]()
+    var imageUuid: String?
     
     let categories = ["Clothing", "Bags", "Footwear", "Eyewear", "Accessories", "Jewelry", "Other"]
     let genders = ["Male", "Female", "Both"]
     let times = ["1 Hour", "3 Hours", "6 Hours", "12 Hours", "1 Day", "3 Days", "7 Days"]
+    let conditions = ["New", "Open Box", "Barely Used", "Used"]
     
     var categoryPickerView = UIPickerView()
     var genderPickerView = UIPickerView()
     var timePickerView = UIPickerView()
+    var conditionPickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,29 +59,7 @@ class AddProductTableViewController: UITableViewController, UIImagePickerControl
         // Set the number of pages on the page control
         pageControl.numberOfPages = imageViews.count
         
-        categoryTextField.inputView = categoryPickerView
-        genderTextField.inputView = genderPickerView
-        timeTextField.inputView = timePickerView
-        
-        categoryTextField.placeholder = "Select Category"
-        genderTextField.placeholder = "Select Gender"
-        timeTextField.placeholder = "Select Time Period"
-        
-        categoryTextField.textAlignment = .center
-        genderTextField.textAlignment = .center
-        timeTextField.textAlignment = .center
-        
-        categoryPickerView.delegate = self
-        categoryPickerView.dataSource = self
-        genderPickerView.delegate = self
-        genderPickerView.dataSource = self
-        timePickerView.delegate = self
-        timePickerView.dataSource = self
-        
-        
-        categoryPickerView.tag = 1
-        genderPickerView.tag = 2
-        timePickerView.tag = 3
+        assignPickerViews()
         
         submitItem.isEnabled = false
         
@@ -103,6 +86,83 @@ class AddProductTableViewController: UITableViewController, UIImagePickerControl
         
         self.present(alert, animated: true)
     }
+    
+  
+    @IBAction func checkMarkButtonPressed(_ sender: Any) {
+        
+        guard
+            let categoryTextField = categoryTextField.text, !categoryTextField.isEmpty
+        else {
+            let alert = UIAlertController(
+                title: "Invalid Request",
+                message: "Please fill empty fields",
+                preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        validated.toggle()
+    }
+    
+    var validated = false {
+        didSet {
+            checkMark.isSelected = validated
+            submitItem.isEnabled = validated
+        }
+    }
+    
+    //Save products
+    @IBAction func submitItemButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(
+            title: "Submit Listing",
+            message: "Are you sure you want to submit the listing?",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { UIAlertAction in
+            guard let description = self.descriptionTextField.text, let productName = self.productNameTextField.text else {
+                print("Error: Some fields are empty")
+                return
+            }
+            var data = [
+                "productName": self.productNameTextField.text,
+                "description": self.descriptionTextField.text,
+                "startingPrice": self.startingPriceTextField.text,
+                "minimumIncrement": self.minimumIncrementTextField.text,
+                "buyItNow": self.buyItNowTextField.text,
+                "timePeriod": self.timeTextField.text,
+                "category": self.categoryTextField.text,
+                "gender": self.genderTextField.text,
+                "brand": self.brandTextField.text,
+                "condition": self.conditionTextField.text,
+                "imageUuid": self.imageUuid
+            ]
+            
+            // Create a database reference
+            let databaseRef = Database.Products.products.childByAutoId()
+
+            // Create a child reference for "products" node
+            let productsRef = databaseRef
+
+            productsRef.setValue(data) { (error, ref) in
+                if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                } else {
+                    print("Data saved successfully!")
+                    self.performSegue(withIdentifier: "unwindToHome", sender: nil)
+                }
+            }
+        })
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        self.present(alert, animated: true)
+
+    }
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
@@ -133,156 +193,124 @@ class AddProductTableViewController: UITableViewController, UIImagePickerControl
         
         // Update the number of pages on the page control
         pageControl.numberOfPages = imageViews.count
+        guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+            print("Failed to get JPEG data from image")
+            return
+        }
+        
+        let storageRef = Storage.storage().reference().child("images/\(UUID().uuidString).jpg")
+        guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+            print("Failed to convert UIImage to Data")
+            return
+        }
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        storageRef.putData(imageData, metadata: metadata) { (metadata, error) in
+            if let error = error {
+                print("Error uploading image: \(error.localizedDescription)")
+            } else {
+                print("Image uploaded successfully")
+                // You can access the download URL of the uploaded image like this:
+                storageRef.downloadURL(completion: { (url, error) in
+                    if let error = error {
+                        print("Error retrieving download URL: \(error.localizedDescription)")
+                    } else {
+                        guard let downloadURL = url else {
+                            print("Download URL not found")
+                            return
+                        }
+                        self.imageUuid = downloadURL.absoluteString // Update imageUuid with the download URL
+                        
+                        print("Image uploaded successfully! Download URL: \(downloadURL)")
+                        print("Download URL: \(downloadURL.absoluteString)")
+                    }
+                })
+            }
+        }
     }
+    
+    
+    
     
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width)
     }
- 
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return categories.count
+        case 2:
+            return genders.count
+        case 3:
+            return times.count
+        case 4:
+            return conditions.count
+        default:
+            return 1
+        }
+    }
     
-    @IBAction func checkMarkButtonPressed(_ sender: Any) {
-        
-        guard
-            let categoryTextField = categoryTextField.text, !categoryTextField.isEmpty
-        else {
-            let alert = UIAlertController(
-                title: "Invalid Request",
-                message: "Please fill empty fields",
-                preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel))
-            
-            self.present(alert, animated: true)
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return categories[row]
+        case 2:
+            return genders[row]
+        case 3:
+            return times[row]
+        case 4:
+            return conditions[row]
+        default:
+            return "Picker data not found."
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            categoryTextField.text = categories[row]
+            categoryTextField.resignFirstResponder()
+        case 2:
+            genderTextField.text = genders[row]
+            genderTextField.resignFirstResponder()
+        case 3:
+            timeTextField.text = times[row]
+            timeTextField.resignFirstResponder()
+        case 4:
+            conditionTextField.text = conditions[row]
+            conditionTextField.resignFirstResponder()
+        default:
             return
         }
-        
-        validated.toggle()
     }
     
-    var validated = false {
-        didSet {
-            checkMark.isSelected = validated
-            submitItem.isEnabled = validated
-        }
+    func assignPickerViews(){
+        categoryTextField.inputView = categoryPickerView
+        genderTextField.inputView = genderPickerView
+        timeTextField.inputView = timePickerView
+        conditionTextField.inputView = conditionPickerView
+        
+        //        categoryTextField.placeholder = "Select Category"
+        //        genderTextField.placeholder = "Select Gender"
+        //        timeTextField.placeholder = "Select Time Period"
+        
+        categoryTextField.textAlignment = .left
+        genderTextField.textAlignment = .left
+        timeTextField.textAlignment = .left
+        categoryPickerView.delegate = self
+        categoryPickerView.dataSource = self
+        genderPickerView.delegate = self
+        genderPickerView.dataSource = self
+        timePickerView.delegate = self
+        timePickerView.dataSource = self
+        conditionPickerView.delegate = self
+        conditionPickerView.dataSource = self
+        categoryPickerView.tag = 1
+        genderPickerView.tag = 2
+        timePickerView.tag = 3
+        conditionPickerView.tag = 4
     }
-
-    //Save products
-    @IBAction func submitItemButtonPressed(_ sender: Any) {
+}
         
-        let alert = UIAlertController(
-            title: "Submit Listing",
-            message: "Are you sure you want to submit the listing?",
-            preferredStyle: .alert
-        )
-
-        alert.addAction(UIAlertAction(title: "OK", style: .default) { UIAlertAction in
-            guard let description = self.descriptionTextField.text, let productName = self.productNameTextField.text else {
-                print("Error: Some fields are empty")
-                return
-            }
-            var data = [
-                "productName": self.productNameTextField.text,
-                "description": self.descriptionTextField.text,
-                "startingPrice": self.startingPriceTextField.text,
-                "minimumIncrement": self.minimumIncrementTextField.text,
-                "buyItNow": self.buyItNowTextField.text,
-                "timePeriod": self.timeTextField.text,
-                "category": self.categoryTextField.text,
-                "gender": self.genderTextField.text,
-                "brand": self.brandTextField.text,
-                "condition": self.conditionTextField.text
-            ]
-            
-            // Create a database reference
-            let databaseRef = Database.Products.products.childByAutoId()
-
-            // Create a child reference for "products" node
-            let productsRef = databaseRef
-
-            // Set the values to be saved in the database
-            // data = ["name": name, "email": email]
-
-            // Save the data to the new child node
-            productsRef.setValue(data) { (error, ref) in
-                if let error = error {
-                    print("Error: \(error.localizedDescription)")
-                } else {
-                    print("Data saved successfully!")
-                    self.performSegue(withIdentifier: "unwindToHome", sender: nil)
-                }
-            }
-        })
-
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-
-        self.present(alert, animated: true)
-
-    }
-        
-       
-    
-
-    
-
-//        let storageRef = Storage.storage().reference().child("images/\(UUID().uuidString).jpg")
-//
-//        guard let imageData = image.jpegData(compressionQuality: 0.75) else {
-//            print("Error: Failed to get image data")
-//            return
-//        }
-//
-//        let uploadTask = storageRef.putData(imageData, metadata: nil) { (metadata, error) in
-//            guard let metadata = metadata else {
-//                print("Error: \(error?.localizedDescription ?? "Unknown error")")
-//                return
-//            }
-//
-//            storageRef.downloadURL { (url, error) in
-//                guard let downloadURL = url else {
-//                    print("Error: \(error?.localizedDescription ?? "Unknown error")")
-//                    return
-//                }
-    
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            switch pickerView.tag {
-            case 1:
-                return categories.count
-            case 2:
-                return genders.count
-            case 3:
-                return times.count
-            default:
-                return 1
-            }
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-            switch pickerView.tag {
-            case 1:
-                return categories[row]
-            case 2:
-                return genders[row]
-            case 3:
-                return times[row]
-            default:
-                return "Picker data not found."
-            }
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-            switch pickerView.tag {
-            case 1:
-                categoryTextField.text = categories[row]
-                categoryTextField.resignFirstResponder()
-            case 2:
-                genderTextField.text = genders[row]
-                genderTextField.resignFirstResponder()
-            case 3:
-                timeTextField.text = times[row]
-                timeTextField.resignFirstResponder()
-            default:
-                return
-            }
-        }
-    }
  
--- SomaApp/LastestProduct/LatestProductTableViewController.swift
+++ SomaApp/LastestProduct/LatestProductTableViewController.swift
@@ -6,97 +6,108 @@ class LatestProductTableViewController: UITableViewController {
 
     var latestProduct: [String: Any]?
     var latestImage: UIImage?
+    var latestProducts: [[String: Any]] = []
+    var latestImages: [UIImage] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        
+        self.latestProducts = []
+        self.latestImages = []
         // Query the database for all products sorted by timestamp in descending order
-        Database.Products.products.queryOrdered(byChild: "timestamp").observeSingleEvent(of: .value, with: { (snapshot) in
-            // Retrieve the most recent product
-            let products = snapshot.children.allObjects as! [DataSnapshot]
-            let mostRecentProduct = products.last
-            guard let product = mostRecentProduct?.value as? [String: Any] else {
-                return
-            }
-            self.latestProduct = product
-
-            print("Latest product: \(self.latestProduct)")
-
-            self.tableView.reloadData() // reload table view data
-
-        }) { (error) in
-            print(error.localizedDescription)
-        }
-
-        // Query the storage for all images sorted by timestamp in descending order
-        let storageRef = Storage.storage().reference().child("images")
-
-        // Query the storage for all images sorted by timestamp in descending order
-        storageRef.list(maxResults: 1, completion: { (result, error) in
-            if let result = result {
-                let images = result.items
-                // Retrieve the latest image
-                if let imageRef = images.first {
-                    imageRef.getData(maxSize: 10 * 1024 * 1024) { (data, error) in
-                        if let error = error {
-                            print("Error downloading image: \(error.localizedDescription)")
-                            return
+        Database.Products.products.queryOrdered(byChild: "timestamp").queryLimited(toLast: 4).observeSingleEvent(of: .value, with: { (snapshot) in
+                    // Retrieve the latest products
+                    let products = snapshot.children.allObjects as! [DataSnapshot]
+                    for productSnapshot in products.reversed() {
+                        guard let product = productSnapshot.value as? [String: Any] else {
+                            continue
                         }
-                        if let data = data, let image = UIImage(data: data) {
-                            // Use the downloaded image
-                            self.latestImage = image
-
-                            print("Latest image: \(self.latestImage)")
-
-                            self.tableView.reloadData() // reload table view data
-                        }
+                        self.latestProducts.append(product)
                     }
+
+                    print("Latest products: \(self.latestProducts)")
+
+                    self.tableView.reloadData() // reload table view data
+
+                }) { (error) in
+                    print(error.localizedDescription)
                 }
-            } else if let error = error {
-                print("Error querying storage: \(error.localizedDescription)")
+
+                // Query the storage for all images sorted by timestamp in descending order
+                let storageRef = Storage.storage().reference().child("images")
+
+                // Query the storage for all images sorted by timestamp in descending order
+                storageRef.list(maxResults: 4, completion: { (result, error) in
+                    if let result = result {
+                        let images = result.items
+                        // Retrieve the latest images
+                        for imageRef in images.reversed() {
+                            imageRef.getData(maxSize: 10 * 1024 * 1024) { (data, error) in
+                                if let error = error {
+                                    print("Error downloading image: \(error.localizedDescription)")
+                                    return
+                                }
+                                if let data = data, let image = UIImage(data: data) {
+                                    // Use the downloaded image
+                                    self.latestImages.append(image)
+
+                                    print("Latest image: \(image)")
+
+                                    self.tableView.reloadData() // reload table view data
+                                }
+                            }
+                        }
+                    } else if let error = error {
+                        print("Error querying storage: \(error.localizedDescription)")
+                    }
+                })
             }
-        })
-    }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! LatestProductTableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! LatestProductTableViewCell
 
-        // Retrieve the latest product
-        guard let latestProduct = self.latestProduct else {
+            // Retrieve the latest products and images
+        guard !self.latestProducts.isEmpty,
+              !self.latestImages.isEmpty else {
             return cell
         }
 
-        let cellWidth = cell.contentView.frame.width
-        let cellHeight = cell.contentView.frame.height
 
-        // Set the product image and time period label for each cell
-        for i in 0..<4 {
-            let x = CGFloat(i % 2) * (cellWidth / 2) + 8
-            let y = CGFloat(i / 2) * (cellHeight / 2.5)  // Add spacing between rows
+            let cellWidth = cell.contentView.frame.width
+            let cellHeight = cell.contentView.frame.height
 
-            // Create the product image view
-            let productImageView = UIImageView(frame: CGRect(x: x, y: y, width: cellWidth / 2 - 15 , height: cellHeight * 0.3))
-            productImageView.contentMode = .scaleAspectFill
-            productImageView.clipsToBounds = true
+            // Display the latest 4 products and images
+            for i in 0..<min(latestProducts.count, 4) {
+                let x = CGFloat(i % 2) * (cellWidth / 2) + 8
+                let y = CGFloat(i / 2) * (cellHeight / 2.5)  // Add spacing between rows
 
-            // Set the product image
-            if let latestImage = self.latestImage {
+                // Create the product image view
+                let productImageView = UIImageView(frame: CGRect(x: x, y: y, width: cellWidth / 2 - 15 , height: cellHeight * 0.3))
+                productImageView.contentMode = .scaleAspectFill
+                productImageView.clipsToBounds = true
+
+                // Set the product image
+                let latestImage = latestImages[i % latestImages.count]
                 productImageView.image = latestImage
+
+                // Create the time period label
+                let timePeriodLabel = UILabel(frame: CGRect(x: x + 4, y: y + productImageView.frame.height + 8, width: cellWidth / 2 - 8, height: 24))
+                timePeriodLabel.font = UIFont.systemFont(ofSize: 16)
+
+                // Set the time period label text
+                let latestProduct = latestProducts[i % latestProducts.count]
+                let timePeriod = latestProduct["timePeriod"] as? String ?? ""
+                timePeriodLabel.text = timePeriod
+
+                cell.contentView.addSubview(productImageView)
+                cell.contentView.addSubview(timePeriodLabel)
             }
 
-            // Create the time period label
-            let timePeriodLabel = UILabel(frame: CGRect(x: x + 4, y: y + productImageView.frame.height + 8, width: cellWidth / 2 - 8, height: 24))
-            timePeriodLabel.font = UIFont.systemFont(ofSize: 16)
-            let timePeriod = latestProduct["timePeriod"] as? String ?? ""
-            timePeriodLabel.text = timePeriod
-
-            cell.contentView.addSubview(productImageView)
-            cell.contentView.addSubview(timePeriodLabel)
+            return cell
         }
 
-        return cell
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
